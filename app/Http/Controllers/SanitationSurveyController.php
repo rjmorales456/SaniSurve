60a4b3e22fc0c2059555507f8620c05c5406614b
AceVG <theacevg@gmail.com>
2024-02-15 14:50:24 +0800

    Cleaned up SanitarySurveyController and made DispoofBio and NonBio required

--- app/Http/Controllers/SanitationSurveyController.php
+++ app/Http/Controllers/SanitationSurveyController.php
@@ -32,8 +32,8 @@ class SanitationSurveyController extends Controller
             'shared_with_other_household' => 'required|string',
             'household_practices_waste_segregation' => 'required|string',
             'collected_by_city_collection_and_disposal_system' => 'required|string',
-            'disposal_of_biodegradable' => 'nullable|array', 
-            'disposal_of_non_biodegradable' => 'nullable|array',
+            'disposal_of_biodegradable' => 'required|array', 
+            'disposal_of_non_biodegradable' => 'required|array',
             'recycling_and_reusing' => 'required|string',
             'depth' => 'nullable|string',
             'years_constructed' => 'nullable|string',
@@ -47,36 +47,17 @@ class SanitationSurveyController extends Controller
         
         ]);
 
+
+        // Encode Arrays into Json
+        $validatedData['specified_method_for_excreta_disposal'] = json_encode($validatedData['specified_method_for_excreta_disposal']);
+        $validatedData['disposal_of_biodegradable'] = json_encode($validatedData['disposal_of_biodegradable']);
+        $validatedData['disposal_of_non_biodegradable'] = json_encode($validatedData['disposal_of_non_biodegradable']);
+
+
         // Process image upload
         // $imageName = 'sanitation_survey_image_' . time() . '_' . uniqid() . '.' . $request->sanitation_survey_image_name->extension(); // Generate unique filename
         // $request->sanitation_survey_image_name->move(public_path('sanitation_survey_images'), $imageName); // Move uploaded image to storage directory
 
-        // Check if the 'disposal_of_biodegradable' field exists
-        if (isset($validatedData['disposal_of_biodegradable'])) {
-            // Convert the array to a string before inserting into the database
-            $validatedData['disposal_of_biodegradable'] = json_encode($validatedData['disposal_of_biodegradable']);
-        } else {
-            // If the field is not present or empty, set it to null
-            $validatedData['disposal_of_biodegradable'] = null;
-        }
-
-        // Check if the 'disposal_of_non_biodegradable' field exists
-        if (isset($validatedData['disposal_of_non_biodegradable'])) {
-            // Convert the array to a string before inserting into the database
-            $validatedData['disposal_of_non_biodegradable'] = json_encode($validatedData['disposal_of_non_biodegradable']);
-        } else {
-            // If the field is not present or empty, set it to null
-            $validatedData['disposal_of_non_biodegradable'] = null;
-        }
-        
-        // Check if the specified_method_for_excreta_disposal field is present and not empty
-        if (isset($validatedData['specified_method_for_excreta_disposal'])) {
-            // Convert the array to a string before inserting into the database
-            $validatedData['specified_method_for_excreta_disposal'] = json_encode($validatedData['specified_method_for_excreta_disposal']);
-        } else {
-            // If the field is not present or empty, set it to null
-            $validatedData['specified_method_for_excreta_disposal'] = null;
-        } 
 
         // Create a new sanitation survey instance
         $sanitationSurvey = SanitationSurvey::create($validatedData);
@@ -112,8 +93,8 @@ class SanitationSurveyController extends Controller
             'shared_with_other_household' => 'required|string',
             'household_practices_waste_segregation' => 'required|string',
             'collected_by_city_collection_and_disposal_system' => 'required|string',
-            'disposal_of_biodegradable' => 'nullable|array', 
-            'disposal_of_non_biodegradable' => 'nullable|array',
+            'disposal_of_biodegradable' => 'required|array', 
+            'disposal_of_non_biodegradable' => 'required|array',
             'recycling_and_reusing' => 'required|string',
             'depth' => 'nullable|string',
             'years_constructed' => 'nullable|string',
