57e90f19f7d626728758c66e18e34189c2af979e
AceVG <theacevg@gmail.com>
2024-02-16 10:12:27 +0800

    created a seeder that inserts an admin account ot the database

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'System',
+            'last_name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('@dminP@ssword'),
+            'role' => 1, 
+            'contact_number' => 'adminContactNumber',
+        ]);
+    }
+}
